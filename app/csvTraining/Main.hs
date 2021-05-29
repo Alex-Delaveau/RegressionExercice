c224cca403ff1781c62ea81d093906b0f732fb29
Alex Delaveau <alex.delaveau@etu.u-bordeaux.fr>
2021-05-29 15:06:27 +0200

    Integrated Csv parse to regression

--- app/csvTraining/Main.hs
+++ app/csvTraining/Main.hs
@@ -8,9 +8,12 @@ main = do
     let fileName = "test.csv"
     input <- readFile fileName
     let csv = parseCSV fileName input
-    either handleError doWork csv
-handleError csv = putStrLn "error parsing"
-doWork csv = print $ toData csv
+    case csv of 
+        Left e -> print ("Parse error" ++ show e)
+        Right y -> print "Parse Ok"
+    let Right dataString = csv
+    let dataFloat = toData dataString
+
 
 toData :: [[String]] -> [([Float], Float)]
 toData x = map (\[xs,x] -> ([xs],x)) $ map (map (read::String->Float)) $ tail x
